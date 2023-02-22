a00cc68724e34bee04ba0bbb2799571174d9c7c9
Chandler <chandlercoven@florida.usa.com>
2023-02-22 10:47:07 -0500

    updated section text

--- src/components/sections/offer.vue
+++ src/components/sections/offer.vue
@@ -4,16 +4,17 @@
             <img src="../../assets/offer.png" alt="Offerings">
             
             <div class="grid gap-6 text-custom-blue h-fit">
-                <h1 class="text-xl font-bold">We offer the best services</h1>
+                <h1 class="text-xl font-bold">The Lead Exchange - Your Partner for Successful Insurance Business</h1>
 
-                <p class="w-[90%]">An innovative leader, Insurance Express is on the cutting edge, utilizing state of the art technology, allowing us to stay ahead of market trends while delivering seamless rapid response solutions in an ever-evolving market place. Insurance Express is committed to providing our clients the highest quality of customer service to ensure that the purchase and servicing of all policies is executed smoothly.</p>
+                <p class="w-[90%]">The Lead Exchange is a trusted partner for insurance agents in Florida, providing a comprehensive lead referral program that helps agents expand their
+                     business and increase revenue. Our state-of-the-art technology and extensive network of insurance markets enable us to deliver seamless, rapid response solutions to meet the ever-evolving needs of the market.</p>
             
                 <div class="grid gap-2">
-                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Less overhead cost</p>
-                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Competitive rates</p>
-                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Higher close ratio</p>
-                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Higher commissions</p>
-                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Higher client retention</p>
+                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Expand your business with our lead referral program that provides access to every property and casualty insurance market in Florida.</p>
+                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Place business that you cannot on your own, and earn commissions for as long as the business remains on our books.</p>
+                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Enjoy less overhead cost and highly competitive rates, thanks to our streamlined approach and cutting-edge technology.</p>
+                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Benefit from a higher close ratio and client retention, thanks to our commitment to providing the highest quality of customer service.</p>
+                    <p class="flex font-medium gap-4"><CheckCircleIcon class="h-6" />Trust in our expertise and experience to help you stay ahead of market trends and succeed in the ever-changing insurance industry.</p>
                 </div>
 
                 <RouterLink to="/get-appointed" class="w-fit text-custom-blue font-medium border-2 border-custom-blue py-4 px-6 rounded-3xl hover:text-white hover:bg-custom-blue">Get Appointed</RouterLink>
